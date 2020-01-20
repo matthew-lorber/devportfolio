d77ffa0fd1b6901f1f105082a4fe7de65211eade
Matt Lorber <matthew.lorber@outlook.com>
2020-01-20 08:42:35 -0600

    simplified (&fixed) page transitions using jQuery and cubic bez ease

--- assets/js/mouse.js
+++ assets/js/mouse.js
@@ -26,20 +26,20 @@ function parallaxFx() {
     let ctr = {x:window.innerWidth/2, y:window.innerHeight/2};
     let d = {x:-(ctr.x - mm.x), y:-(ctr.y - mm.y)};
     let r = {x:-d.x/20,y:d.y/20};
-    $('#test').css({'transform':'translateX('+10*d.x/ctr.x+'px) translateY('+10*d.y/ctr.y+'px) rotateY('+r.x+'deg) rotateX('+r.y+'deg)'});
-    $('#test2').css({'transform':'translateX('+40*d.x/ctr.x+'px) translateY('+40*d.y/ctr.y+'px) rotateY('+r.x+'deg) rotateX('+r.y+'deg)'});
+    $('h1').css({'transform':'translateX('+10*d.x/ctr.x+'px) translateY('+10*d.y/ctr.y+'px) rotateY('+r.x+'deg) rotateX('+r.y+'deg)'});
+    $('h2').css({'transform':'translateX('+40*d.x/ctr.x+'px) translateY('+40*d.y/ctr.y+'px) rotateY('+r.x+'deg) rotateX('+r.y+'deg)'});
 };
 
 // MOUSEDOWN (SCALE)
-window.addEventListener("mousedown",mouseFx);
+window.addEventListener("mousedown", mouseFx);
 window.addEventListener("mouseup", ()=> restore());
 
 function mouseFx() {
     $("#mIn").css("transform","scale(1.5)");
-    $("section").css("transform","scale(1.2)");
+    $("h1,h2").css("transform","scale(1.2)");
 }
 
 restore = ()=> {
     $("#mIn").css("transform","scale(1)");
-    $("section").css("transform","scale(1)");
+    $("h1,h2").css("transform","scale(1)");
 }
