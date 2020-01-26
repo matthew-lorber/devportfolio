6444afc005cfaa65998664a86116331295d5042f
Matt Lorber <matthew.lorber@outlook.com>
2020-01-25 18:21:38 -0600

    added back in the dimensionings; NB: hook to event creation

--- assets/js/links.js
+++ assets/js/links.js
@@ -1,24 +1,16 @@
 $("#pm").on("click", function() {
     if ($(this).hasClass("open")) {
         $(this).removeClass("open");
-        anime({
-            targets: "#pmx",
-            rotate: 0
-        });
-        anime({
-            targets: ".link",
-            translateX: 0,
-            translateY: 0
-        });
+        anime({targets: "#pm", width: 40, height: 40});
+        anime({targets: "#pmx", rotate: 0});
+        anime({targets: ".link", translateX: 0, translateY: 0, opacity:0});
     } else {
         $(this).addClass("open");
-        anime({
-            targets: "#pmx",
-            rotate: 45
-        });
-        anime({targets: "#li", translateX: 40, translateY: -40});
-        anime({targets: "#mt", translateX: -40, translateY: -40});
-        anime({targets: "#gh", translateX: 40, translateY: 40});
-        anime({targets: "#cp", translateX: -40, translateY: 40});
+        anime({targets: "#pm", width: 75, height: 75})
+        anime({targets: "#pmx", rotate: 45});
+        anime({targets: "#li", translateX: -50, opacity:1});
+        anime({targets: "#mt", translateX: 50, opacity:1});
+        anime({targets: "#gh", translateY: -50, opacity:1});
+        anime({targets: "#cp", translateY: 50, opacity:1});
     }
 });
